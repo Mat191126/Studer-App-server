bea5d9296360d255a793c73e4c22ccb9d44e1ca3
Mat191126 <matprz11@gmail.com>
2021-02-22 12:00:18 +0100

    Add BaseEntityMethods interface

---
+++ src/main/java/com/codecool/studer/entities/BaseEntityMethods.java
@@ -0,0 +1,8 @@
+package com.codecool.studer.entities;
+
+public interface BaseEntityMethods<K> {
+
+    K getId();
+
+    void setActive(boolean status);
+}
--- src/main/java/com/codecool/studer/entities/Location.java
+++ src/main/java/com/codecool/studer/entities/Location.java
@@ -1,13 +1,15 @@
 package com.codecool.studer.entities;
 
 import com.sun.istack.NotNull;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Data
 @Entity
-public class Location {
+public class Location implements BaseEntityMethods<UUID> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
