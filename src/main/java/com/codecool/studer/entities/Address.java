2762e418ee6c987e12d861640ad1963141e03f6d
Mat191126 <matprz11@gmail.com>
2021-02-23 15:19:41 +0100

    Add Address entity

---
+++ src/main/java/com/codecool/studer/entities/Address.java
@@ -0,0 +1,42 @@
+package com.codecool.studer.entities;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private UUID id;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String streetNumber;
+
+    @NotNull
+    private String town;
+
+    @NotNull
+    private String zipCode;
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    private Location location;
+
+    @NotNull
+    @Column(columnDefinition = "boolean default true")
+    private boolean active;
+}
