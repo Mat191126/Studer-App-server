ff8c07fb9326862cd74fc0b927cc7ca704c54b1e
Mat191126 <matprz11@gmail.com>
2021-02-24 11:51:04 +0100

    Add Location and PlaceType repositories

---
+++ src/main/java/com/company/studer/repositories/LocationRepository.java
@@ -0,0 +1,9 @@
+package com.company.studer.repositories;
+
+import com.company.studer.entities.Location;
+
+import java.util.UUID;
+
+public interface LocationRepository extends CrudRepositoryMethods<Location, UUID> {
+
+}
---
+++ src/main/java/com/company/studer/repositories/PlaceTypeRepository.java
@@ -0,0 +1,7 @@
+package com.company.studer.repositories;
+
+import com.company.studer.entities.PlaceType;
+
+public interface PlaceTypeRepository extends CrudRepositoryMethods<PlaceType, Long> {
+
+}
