98267d1859d72185dab39075a5d74b9c76c9c729
Mat191126 <matprz11@gmail.com>
2021-02-24 12:19:42 +0100

    PlaceType and Location repositories refactor

--- src/main/java/com/company/studer/repositories/PlaceTypeRepository.java
+++ src/main/java/com/company/studer/repositories/PlaceTypeRepository.java
@@ -1,7 +1,9 @@
 package com.company.studer.repositories;
 
 import com.company.studer.entities.PlaceType;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaceTypeRepository extends CrudRepositoryMethods<PlaceType, Long> {
 
 }
