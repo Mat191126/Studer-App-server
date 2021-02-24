e500a700b0a5befc08b0d2b1c30c95ebe3bde1fd
Mat191126 <matprz11@gmail.com>
2021-02-24 13:59:09 +0100

    Add FavouritePlace service

---
+++ src/main/java/com/company/studer/services/FavouritePlaceService.java
@@ -0,0 +1,43 @@
+package com.company.studer.services;
+
+import com.company.studer.entities.FavouritePlace;
+import com.company.studer.repositories.CrudRepositoryMethods;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class FavouritePlaceService extends CrudService<FavouritePlace, UUID> {
+
+    @Autowired
+    public FavouritePlaceService(CrudRepositoryMethods<FavouritePlace, UUID> repository) {
+        super(repository);
+    }
+
+    @Override
+    public boolean update(FavouritePlace newFavouritePlaces) {
+        Optional<FavouritePlace> oldPlaceOptional = get(newFavouritePlaces.getId());
+        if (oldPlaceOptional.isPresent()) {
+            FavouritePlace oldFavouritePlace = oldPlaceOptional.get();
+            oldFavouritePlace.setUser(newFavouritePlaces.getUser());
+            oldFavouritePlace.setPlace(newFavouritePlaces.getPlace());
+            add(oldFavouritePlace);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public final boolean delete(UUID id) {
+        Optional<FavouritePlace> object = get(id);
+        if (object.isPresent()) {
+            FavouritePlace oldObject = object.get();
+            oldObject.setActive(false);
+            repository.save(oldObject);
+            return true;
+        }
+        return false;
+    }
+}
