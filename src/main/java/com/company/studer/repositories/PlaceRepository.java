1c847d92eb19bf053cdc59f18fa19836e643bb4d
Mat191126 <matprz11@gmail.com>
2021-02-25 15:26:31 +0100

    Refactor of FavouritePlaceRepository, PlaceRepository and PlaceTypeRepository, deleted SelectableByUserIdAndActive.java

--- src/main/java/com/company/studer/repositories/PlaceRepository.java
+++ src/main/java/com/company/studer/repositories/PlaceRepository.java
@@ -1,11 +1,14 @@
 package com.company.studer.repositories;
 
 import com.company.studer.entities.Place;
+import com.company.studer.entities.PlaceType;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface PlaceRepository extends CrudRepositoryMethods<Place, UUID> {
+
+    Iterable<Place> getPlaceByActiveAndPlaceTypesIn(boolean active, Iterable<PlaceType> placeTypes);
 
 }
--- src/main/java/com/company/studer/repositories/SelectableByUserIdAndActive.java
+++
@@ -1,9 +0,0 @@
-package com.company.studer.repositories;
-
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface SelectableByUserIdAndActive<T, V> {
-
-    Iterable<T> getByUserIdAndActive(V id, boolean active);
-}
