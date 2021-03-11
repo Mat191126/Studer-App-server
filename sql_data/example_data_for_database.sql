8ea58f2f640a703a8b8003452702ffa53441713a
Mat191126 <matprz11@gmail.com>
2021-03-11 12:01:39 +0100

    Database data refactor

--- sql_data/example_data_for_database.sql
+++ sql_data/example_data_for_database.sql
@@ -1,49 +1,78 @@
-INSERT INTO location (id, latitude, longitude)
-VALUES ('235524ea-76a6-11eb-9439-0242ac130002', 23.45609, 34.8288),
-       ('2355272e-76a6-11eb-9439-0242ac130002', 102.72563, 234.814),
-       ('2355281e-76a6-11eb-9439-0242ac130002', 12.125, 5.172654),
-       ('23552b02-76a6-11eb-9439-0242ac130002', 274.28, 1736.1873),
-       ('23552bca-76a6-11eb-9439-0242ac130002', 18.7, 127.4),
-       ('23552c88-76a6-11eb-9439-0242ac130002', 8273.7, 1263.1763),
-       ('23552d46-76a6-11eb-9439-0242ac130002', 173264.13478, 27.4),
-       ('23552e04-76a6-11eb-9439-0242ac130002', 7643.98754, 4.3764);
+INSERT INTO location (id, active, latitude, longitude)
+VALUES ('235524ea-76a6-11eb-9439-0242ac130002', true, 50.071522, 19.943859),
+       ('2355272e-76a6-11eb-9439-0242ac130002', true, 50.043740, 19.951120),
+       ('2355281e-76a6-11eb-9439-0242ac130002', true, 50.0670, 19.9294),
+       ('23552b02-76a6-11eb-9439-0242ac130002', true, 50.0647, 19.9381),
+       ('23552bca-76a6-11eb-9439-0242ac130002', true, 50.0584, 50.0584),
+       ('23552c88-76a6-11eb-9439-0242ac130002', true, 50.0599, 19.9107),
+       ('23552d46-76a6-11eb-9439-0242ac130002', true, 50.0418, 19.9495),
+       ('23552e04-76a6-11eb-9439-0242ac130002', true, 50.0495, 19.9169),
+       ('23552c54-76a6-11eb-9439-0242ac130002', true, 50.0528, 19.9439),
+       ('23552c27-76a6-11eb-9439-0242ac130002', true, 50.0603, 19.9378),
+       ('23552c28-76a6-11eb-9439-0242ac130002', true, 50.0633, 19.9370),
+       ('235usr1t-76a6-11eb-9439-0242ac130002', true, 50.0395, 19.9412),
+       ('235usr2a-76a6-11eb-9439-0242ac130002', true, 50.0203, 19.9737);
 
-INSERT INTO users (id, email, first_name, last_name, password, user_role, location_id)
-VALUES ('b388fa0a-76a6-11eb-9439-0242ac130002', 'tom@gmail.com', 'Tom', 'Clark', 'asd', 'USER', '235524ea-76a6-11eb-9439-0242ac130002'),
-       ('b388fcbc-76a6-11eb-9439-0242ac130002', 'ann@onet.pl', 'Ann', 'Smith','asd', 'USER', '2355272e-76a6-11eb-9439-0242ac130002');
+INSERT INTO users (id, active, email, first_name, last_name, password, user_role, location_id)
+VALUES ('b388fa0a-76a6-11eb-9439-0242ac130002', true, 'tom@gmail.com', 'Tom', 'Clark', 'asd', 'USER', '235usr1t-76a6-11eb-9439-0242ac130002'),
+       ('b388fcbc-76a6-11eb-9439-0242ac130002', true, 'ann@onet.pl', 'Ann', 'Smith','asd', 'USER', '235usr2a-76a6-11eb-9439-0242ac130002');
 
-INSERT INTO place_type (id, type)
-VALUES (1001, 'LIBRARY'),
-       (1002, 'BOOKSTORE'),
-       (1003, 'MILK BAR');
+INSERT INTO place_type (id, active, type)
+VALUES (1001, true, 'LIBRARIES'),
+       (1002, true, 'QUITE_SPACES'),
+       (1003, true, 'COPY_PLACES'),
+       (1004, true, 'INEXPENSIVE_RESTAURANTS'),
+       (1005, true, 'COFFEE_SHOPS'),
+       (1006, true, 'STUDENT_PUBS'),
+       (1007, true, 'MOVIE_THEATERS'),
+       (1008, true, 'BOOKSTORES'),
+       (1009, true, 'DRUGSTORES'),
+       (1010, true, 'PARKS_GREEN_SPACES'),
+       (1011, true, 'PSYCHOTHERAPISTS');
 
-INSERT INTO address (id, street, street_number, town, zip_code, location_id)
-VALUES ('ed14e978-76a6-11eb-9439-0242ac130002', 'Narrow', '5/4', 'Cracow', '32-045', '2355281e-76a6-11eb-9439-0242ac130002'),
-       ('ed14ebe4-76a6-11eb-9439-0242ac130002', 'Short', '23', 'Cracow', '32-052', '23552b02-76a6-11eb-9439-0242ac130002'),
-       ('ed14ecca-76a6-11eb-9439-0242ac130002', 'East', '125', 'Cracow', '32-654', '23552bca-76a6-11eb-9439-0242ac130002'),
-       ('ed14ed88-76a6-11eb-9439-0242ac130002', 'Cold', '5A', 'Cracow', '31-548', '23552c88-76a6-11eb-9439-0242ac130002'),
-       ('ed14ee64-76a6-11eb-9439-0242ac130002', 'St. Paul', '67B/3', 'Cracow', '32-432', '23552d46-76a6-11eb-9439-0242ac130002'),
-       ('ed14ef22-76a6-11eb-9439-0242ac130002', 'Sunny Square', '5', 'Cracow', '32-004', '23552e04-76a6-11eb-9439-0242ac130002');
+INSERT INTO address (id, active, street, street_number, town, zip_code, location_id)
+VALUES ('ed14e978-76a6-11eb-9439-0242ac130002', true, 'Warszawska', '24', 'Kraków', '31-155', '235524ea-76a6-11eb-9439-0242ac130002'),
+       ('ed14ebe4-76a6-11eb-9439-0242ac130002', true, 'Węgierska', '1', 'Kraków', '30-535', '2355272e-76a6-11eb-9439-0242ac130002'),
+       ('ed14ecca-76a6-11eb-9439-0242ac130002', true, 'Stefana Batorego', '25', 'Kraków', '31-135', '2355281e-76a6-11eb-9439-0242ac130002'),
+       ('ed14ed88-76a6-11eb-9439-0242ac130002', true, 'Sławkowska', '24A', 'Kraków', '31-014', '23552b02-76a6-11eb-9439-0242ac130002'),
+       ('ed14ee64-76a6-11eb-9439-0242ac130002', true, 'Felicjanek', '4', 'Kraków', '31-104', '23552bca-76a6-11eb-9439-0242ac130002'),
+       ('ed14ef22-76a6-11eb-9439-0242ac130002', true, '-', '-', 'Kraków', '33-332', '23552c88-76a6-11eb-9439-0242ac130002'),
+       ('ed14ef14-76a6-11eb-9439-0242ac130002', true, 'Jana Zamoyskiego', '-', 'Kraków', '33-332', '23552d46-76a6-11eb-9439-0242ac130002'),
+       ('ed14ef34-76a6-11eb-9439-0242ac130002', true, 'Praska', '-', 'Kraków', '33-332', '23552e04-76a6-11eb-9439-0242ac130002'),
+       ('ed14ag35-76a6-11eb-9439-0242ac130002', true, 'Miodowa', '20/3', 'Kraków', '31-055', '23552c54-76a6-11eb-9439-0242ac130002'),
+       ('ed14ah43-76a6-11eb-9439-0242ac130002', true, 'Rynek Główny', '13', 'Kraków', '31-042', '23552c27-76a6-11eb-9439-0242ac130002'),
+       ('ed14aj11-76a6-11eb-9439-0242ac130002', true, 'Sławkowska', '4', 'Kraków', '31-014','23552c28-76a6-11eb-9439-0242ac130002');
 
-INSERT INTO place (id, description, name, address_id)
-VALUES ('f166745e-76a8-11eb-9439-0242ac130002', 'You can eat delicious dumplings with your friends', 'DumplingWorld', 'ed14e978-76a6-11eb-9439-0242ac130002'),
-       ('f1667760-76a8-11eb-9439-0242ac130002', 'We have English speaking staff and delicious pancakes', 'Pancaksy', 'ed14ebe4-76a6-11eb-9439-0242ac130002'),
-       ('f1667878-76a8-11eb-9439-0242ac130002', 'Read your book in silence', 'East Library', 'ed14ecca-76a6-11eb-9439-0242ac130002'),
-       ('f166795e-76a8-11eb-9439-0242ac130002', 'Have problem with buying books? Come to our store!', 'BookStore', 'ed14ed88-76a6-11eb-9439-0242ac130002'),
-       ('f1667a30-76a8-11eb-9439-0242ac130002', 'Cosy place for reading? Its here!', 'Cosy Books', 'ed14ee64-76a6-11eb-9439-0242ac130002'),
-       ('f166afaa-76a8-11eb-9439-0242ac130002', 'Our staff will help you to buy needed books', 'BookShelf', 'ed14ef22-76a6-11eb-9439-0242ac130002');
+INSERT INTO place (id, active, description, name, address_id)
+VALUES ('f166745e-76a8-11eb-9439-0242ac130002', true, 'You can copy everything you want!', 'KSERO Kraków 6gr - KAMPUS PK', 'ed14e978-76a6-11eb-9439-0242ac130002'),
+       ('f1667760-76a8-11eb-9439-0242ac130002', true, 'We have English speaking staff', 'Ksero Serwis Kraków Drukarnia Druk Skan Wielkoformatowy', 'ed14ebe4-76a6-11eb-9439-0242ac130002'),
+       ('f1667878-76a8-11eb-9439-0242ac130002', true, 'Copy and not only', 'Ksero i druk ATLANTYDA', 'ed14ecca-76a6-11eb-9439-0242ac130002'),
+       ('f166795e-76a8-11eb-9439-0242ac130002', true, 'Have problem with buying English books? Come to our store!', 'American Bookstore Księgarnia Amerykańska', 'ed14ed88-76a6-11eb-9439-0242ac130002'),
+       ('f1667a30-76a8-11eb-9439-0242ac130002', true, 'the best place to buy English books', 'Massolit Books', 'ed14ee64-76a6-11eb-9439-0242ac130002'),
+       ('f166afaa-76a8-11eb-9439-0242ac130002', true, 'Big area with grass', 'Błonia', 'ed14ef22-76a6-11eb-9439-0242ac130002'),
+       ('f166afab-76a8-11eb-9439-0242ac130002', true, 'Nice park with playground', 'Park Bednarskiego', 'ed14ef14-76a6-11eb-9439-0242ac130002'),
+       ('f166afdb-76a8-11eb-9439-0242ac130002', true, 'Green park in city centre', 'Park Dębnicki', 'ed14ef34-76a6-11eb-9439-0242ac130002'),
+       ('f166agh5-76a8-11eb-9439-0242ac130002', true, 'You can listen to ska, reggae, punku i rockabilly', 'PUB Propaganda', 'ed14ag35-76a6-11eb-9439-0242ac130002'),
+       ('f16677ef-76a8-11eb-9439-0242ac130002', true, 'Music club', 'Louis Music Club & Pub', 'ed14ah43-76a6-11eb-9439-0242ac130002'),
+       ('f16678tg-76a8-11eb-9439-0242ac130002', true, 'Pub with great atmosphere', 'Free Pub', 'ed14aj11-76a6-11eb-9439-0242ac130002');
 
 INSERT INTO place_types_list (place_id, place_type_id)
 VALUES ('f166745e-76a8-11eb-9439-0242ac130002', 1003),
        ('f1667760-76a8-11eb-9439-0242ac130002', 1003),
-       ('f1667878-76a8-11eb-9439-0242ac130002', 1001),
-       ('f166795e-76a8-11eb-9439-0242ac130002', 1002),
-       ('f1667a30-76a8-11eb-9439-0242ac130002', 1001),
-       ('f166afaa-76a8-11eb-9439-0242ac130002', 1002);
+       ('f1667878-76a8-11eb-9439-0242ac130002', 1003),
+       ('f166795e-76a8-11eb-9439-0242ac130002', 1008),
+       ('f1667a30-76a8-11eb-9439-0242ac130002', 1008),
+       ('f1667a30-76a8-11eb-9439-0242ac130002', 1005),
+       ('f166afaa-76a8-11eb-9439-0242ac130002', 1010),
+       ('f166afab-76a8-11eb-9439-0242ac130002', 1010),
+       ('f166afdb-76a8-11eb-9439-0242ac130002', 1010),
+       ('f166agh5-76a8-11eb-9439-0242ac130002', 1006),
+       ('f16677ef-76a8-11eb-9439-0242ac130002', 1006),
+       ('f16678tg-76a8-11eb-9439-0242ac130002', 1006);
 
-INSERT INTO favourite_place (id, place_id, user_id)
-VALUES ('a1327e94-76ab-11eb-9439-0242ac130002', 'f166745e-76a8-11eb-9439-0242ac130002', 'b388fa0a-76a6-11eb-9439-0242ac130002'),
-       ('a1328326-76ab-11eb-9439-0242ac130002', 'f166745e-76a8-11eb-9439-0242ac130002', 'b388fcbc-76a6-11eb-9439-0242ac130002'),
-       ('a1328420-76ab-11eb-9439-0242ac130002', 'f1667878-76a8-11eb-9439-0242ac130002', 'b388fa0a-76a6-11eb-9439-0242ac130002'),
-       ('a13284f2-76ab-11eb-9439-0242ac130002', 'f1667a30-76a8-11eb-9439-0242ac130002', 'b388fa0a-76a6-11eb-9439-0242ac130002'),
-       ('a13285b0-76ab-11eb-9439-0242ac130002', 'f166afaa-76a8-11eb-9439-0242ac130002', 'b388fcbc-76a6-11eb-9439-0242ac130002');
+INSERT INTO favourite_place (id, active, place_id, user_id)
+VALUES ('a1327e94-76ab-11eb-9439-0242ac130002', true, 'f166745e-76a8-11eb-9439-0242ac130002', 'b388fa0a-76a6-11eb-9439-0242ac130002'),
+       ('a1328326-76ab-11eb-9439-0242ac130002', true, 'f166745e-76a8-11eb-9439-0242ac130002', 'b388fcbc-76a6-11eb-9439-0242ac130002'),
+       ('a1328420-76ab-11eb-9439-0242ac130002', true, 'f1667878-76a8-11eb-9439-0242ac130002', 'b388fa0a-76a6-11eb-9439-0242ac130002'),
+       ('a13284f2-76ab-11eb-9439-0242ac130002', true, 'f1667a30-76a8-11eb-9439-0242ac130002', 'b388fa0a-76a6-11eb-9439-0242ac130002'),
+       ('a13285b0-76ab-11eb-9439-0242ac130002', true, 'f166afaa-76a8-11eb-9439-0242ac130002', 'b388fcbc-76a6-11eb-9439-0242ac130002');
