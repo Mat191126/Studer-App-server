89ce3d62e341a81a32dcefad66bd49e67c09e26d
Mat191126 <matprz11@gmail.com>
2021-05-04 10:47:15 +0200

    Phrase minor refactor

--- src/main/java/com/company/studer/entities/Phrase.java
+++ src/main/java/com/company/studer/entities/Phrase.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +14,16 @@ public class Phrase {
     private String phrase;
     private PhraseCategory category;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Phrase phrase1 = (Phrase) o;
+        return phrase.equals(phrase1.phrase) && category == phrase1.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phrase, category);
+    }
 }
