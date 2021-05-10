148da8e21662b1dc58d59c078edcc5b4038032ec
Mat191126 <matprz11@gmail.com>
2021-05-10 16:58:06 +0200

    Resolved problem with static content

--- src/main/java/com/company/studer/config/WebConfig.java
+++ src/main/java/com/company/studer/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.company.studer.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,4 +14,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
+            {
+                    "classpath:/static/"
+            };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+                .setCachePeriod(3000);
+    }
+
 }
